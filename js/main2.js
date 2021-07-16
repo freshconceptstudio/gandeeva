204549b1b20869f16eab2a78b497a62058508366
Kyle Taylor <kyle@freshconceptstudio.com>
2021-07-15 21:55:38 -0600

    Another Round of Feedback Tweaks
    - Animation Flash
    - Logo Bigger
    - Nav restyling

--- js/main2.js
+++ js/main2.js
@@ -1,41 +1,50 @@
 window.addEventListener('load', () => {
-  var element = document. getElementsByClassName('background')[0];
-  element. classList. add('initial');
+  var element = document.getElementsByClassName('background')[0];
+  element.classList.add('initial');
 
 
-  var element = document. getElementsByClassName('color')[0];
-  element.classList.remove("loaded");
-  element.classList.remove("half");
-  element.classList.remove("done");
-  setTimeout(function(){
-    var element = document. getElementsByClassName('color')[0];
-    element. classList.add('loaded');
-  }, 1000);
-  setTimeout(function(){
-    var element = document. getElementsByClassName('color')[0];
-    element. classList.add('half');
-  }, 1600);
-  setTimeout(function(){
-    var element = document. getElementsByClassName('color')[0];
-    element. classList.add('done');
-  }, 2000);
+  // var element = document.getElementsByClassName('color')[0];
+  // element.classList.remove("loaded");
+  // element.classList.remove("half");
+  // element.classList.remove("done");
+  // setTimeout(function(){
+  //   var element = document.getElementsByClassName('color')[0];
+  //   element.classList.add('loaded');
+  // }, 1000);
+  // setTimeout(function(){
+  //   var element = document.getElementsByClassName('color')[0];
+  //   element.classList.add('half');
+  // }, 3000);
+  // setTimeout(function(){
+  //   var element = document.getElementsByClassName('color')[0];
+  //   element.classList.add('done');
+  // }, 7000);
+  // setTimeout(function(){
+  //   var element = document.getElementsByClassName('color')[0];
+  //   element.classList.remove('transition');
+  // }, 10000);
 
-  setInterval(function(){
-    var element = document. getElementsByClassName('color')[0];
-    element.classList.remove("loaded");
-    element.classList.remove("half");
-    element.classList.remove("done");
-    setTimeout(function(){
-      var element = document. getElementsByClassName('color')[0];
-      element. classList.add('loaded');
-    }, 1000);
-    setTimeout(function(){
-      var element = document. getElementsByClassName('color')[0];
-      element. classList.add('half');
-    }, 1600);
-    setTimeout(function(){
-      var element = document. getElementsByClassName('color')[0];
-      element. classList.add('done');
-    }, 2000);
-  }, 3000);
+  // setInterval(function(){
+  //   var element = document.getElementsByClassName('color')[0];
+  //   element.classList.remove("loaded");
+  //   element.classList.remove("half");
+  //   element.classList.remove("done");
+  //   setTimeout(function(){
+  //     var element = document.getElementsByClassName('color')[0];
+  //     element.classList.add('transition');
+  //     element.classList.add('loaded');
+  //   }, 1000);
+  //   setTimeout(function(){
+  //     var element = document.getElementsByClassName('color')[0];
+  //     element.classList.add('half');
+  //   }, 3000);
+  //   setTimeout(function(){
+  //     var element = document.getElementsByClassName('color')[0];
+  //     element.classList.add('done');
+  //   }, 7000);
+  //   setTimeout(function(){
+  //     var element = document.getElementsByClassName('color')[0];
+  //     element.classList.remove('transition');
+  //   }, 10000);
+  // }, 12000);
 })
