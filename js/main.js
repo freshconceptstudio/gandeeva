c3976984cb6631ea5c01244497083722b6b93ba4
Kyle Taylor <kyle@freshconceptstudio.com>
2021-07-20 08:35:22 -0600

    Contact Form

--- js/main.js
+++ js/main.js
@@ -1,4 +1,13 @@
 window.addEventListener('load', () => {
   var element = document. getElementsByClassName('background')[0];
   element. classList. add('initial');
+
+  close = document.getElementsByClassName('close')[0];
+  close.onclick = function(){closeModal()};
 })
+
+
+function closeModal() {
+  var element = document. getElementsByClassName('overlay')[0];
+  element.classList.remove('active');
+}
