34fecc4a6a5ba13da3e3548f83b55463610c4818
Kyle Taylor <kyle@freshconceptstudio.com>
2021-07-28 13:37:17 -0600

    New leadership style

--- js/main.js
+++ js/main.js
@@ -4,10 +4,22 @@ window.addEventListener('load', () => {
 
   close = document.getElementsByClassName('close')[0];
   close.onclick = function(){closeModal()};
+
+
+  object.onclick = function(){myScript};
 })
 
 
 function closeModal() {
   var element = document. getElementsByClassName('overlay')[0];
   element.classList.remove('active');
+}
+
+function openProfile(e, index) {
+  e.preventDefault();
+  var element = document.getElementsByClassName('leadership-profile')[0];
+  element.classList.add('show');
+
+  var profile_content = document.getElementsByClassName('profile-content')[index];
+  profile_content.classList.add('show');
 }
